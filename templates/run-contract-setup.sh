784d30a653cf80b6be5f74c80dd70dfb66a0674d
John Hilliard <praetoriansentry@gmail.com>
2024-03-21 20:39:15 -0400

    docs: wip

--- templates/run-contract-setup.sh
+++ templates/run-contract-setup.sh
@@ -64,6 +64,8 @@ sed -i 's#http://127.0.0.1:8545#{{.l1_rpc_url}}#' hardhat.config.ts
 npm i
 npx hardhat compile
 
+# shellcheck disable=SC1054,SC1083
+{{if .zkevm_use_gas_token_contract}}
 2>&1 echo "Deploying Gas Token"
 printf "[profile.default]\nsrc = 'contracts'\nout = 'out'\nlibs = ['node_modules']\n" > foundry.toml
 forge build
@@ -73,8 +75,6 @@ forge create --json \
       contracts/mocks/ERC20PermitMock.sol:ERC20PermitMock \
       --constructor-args  "CDK Gas Token" "CDK" "{{.zkevm_l2_admin_address}}" "1000000000000000000000000" > gasToken-erc20.json
 
-# shellcheck disable=SC1054,SC1083
-{{if .zkevm_use_gas_token_contract}}
 # In this case, we'll configure the create rollup parameters to have a gas token
 jq --slurpfile c gasToken-erc20.json '.gasTokenAddress = $c[0].deployedTo' /opt/contract-deploy/create_rollup_parameters.json > /opt/zkevm-contracts/deployment/v2/create_rollup_parameters.json
 # shellcheck disable=SC1056,SC1072,SC1073,SC1009
@@ -93,6 +93,7 @@ mkdir -p /opt/zkevm
 cp /opt/zkevm-contracts/deployment/v2/deploy_*.json /opt/zkevm/
 cp /opt/zkevm-contracts/deployment/v2/genesis.json /opt/zkevm/
 cp /opt/zkevm-contracts/deployment/v2/create_rollup_output.json /opt/zkevm/
+cp /opt/zkevm-contracts/deployment/v2/create_rollup_parameters.json /opt/zkevm/
 cp /opt/contract-deploy/*-config.* /opt/zkevm/
 popd
 
