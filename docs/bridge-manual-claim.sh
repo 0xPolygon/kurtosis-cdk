dd29fb733b82acb7b5b43a10af01dc70981cfdd6
leovct <l009.vincent@gmail.com>
2024-03-28 13:34:52 +0100

    feat: enable to process multiple maual claims

--- docs/bridge-manual-claim.sh
+++ docs/bridge-manual-claim.sh
@@ -23,43 +23,45 @@ echo "Getting the list of deposits..."
 curl -s "$bridge_api_url/bridges/$destination_addr?limit=100&offset=0" | jq > bridge-deposits.json
 cat bridge-deposits.json
 
-# Filter the list of deposits down to the first claimable tx that hasn't already been claimed and is destined for L1
+# Filter the list of deposits down to the claimable txs that hasn't already been claimed and are destined for L1
 echo "Filtering the list of deposits..."
-jq '[.deposits[] | select(.ready_for_claim == true and .claim_tx_hash == "" and .dest_net == '$destination_net')] | .[0]' bridge-deposits.json > claimable-tx.json
-cat claimable-tx.json
-if [ "$(<claimable-tx.json)" = "null" ]; then
-  echo "No deposits found..."
-  exit 1
-fi
+jq '[.deposits[] | select(.ready_for_claim == true and .claim_tx_hash == "" and .dest_net == '$destination_net')]' bridge-deposits.json > claimable-txs.json
+cat claimable-txs.json
 
-# Use the bridge service to get the merkle proof of our deposit
-echo "Getting the merkle proof of our deposit..."
-curr_deposit_cnt="$(jq -r '.deposit_cnt' claimable-tx.json)"
-curr_network_id="$(jq -r '.network_id' claimable-tx.json)"
-curl -s "$bridge_api_url/merkle-proof?deposit_cnt=$curr_deposit_cnt&net_id=$curr_network_id" | jq '.' > proof.json
-cat proof.json
+# Process all the claimable txs
+jq -c '.[]' claimable-txs.json | while IFS= read -r tx; do
+  echo "Processing claimable tx..."
+  echo "$tx"
 
-# Get our variables organized
-in_merkle_proof="$(jq -r -c '.proof.merkle_proof' proof.json | tr -d '"')"
-in_rollup_merkle_proof="$(jq -r -c '.proof.rollup_merkle_proof' proof.json | tr -d '"')"
-in_global_index="$(jq -r '.global_index' claimable-tx.json)"
-in_main_exit_root="$(jq -r '.proof.main_exit_root' proof.json)"
-in_rollup_exit_root="$(jq -r '.proof.rollup_exit_root' proof.json)"
-in_orig_net="$(jq -r '.orig_net' claimable-tx.json)"
-in_orig_addr="$(jq -r '.orig_addr' claimable-tx.json)"
-in_dest_net="$(jq -r '.dest_net' claimable-tx.json)"
-in_dest_addr="$(jq -r '.dest_addr' claimable-tx.json)"
-in_amount="$(jq -r '.amount' claimable-tx.json)"
-in_metadata="$(jq -r '.metadata' claimable-tx.json)"
+  # Use the bridge service to get the merkle proof of our deposit
+  echo "Getting the merkle proof of our deposit..."
+  curr_deposit_cnt="$(echo $tx | jq -r '.deposit_cnt')"
+  curr_network_id="$(echo $tx | jq -r '.network_id')"
+  curl -s "$bridge_api_url/merkle-proof?deposit_cnt=$curr_deposit_cnt&net_id=$curr_network_id" | jq '.' > proof.json
+  cat proof.json
 
-# Generate the call data, this is useful just to examine what the call will look loke
-echo "Generating the call data for the bridge claim tx..."
-cast calldata "$claim_sig" "$in_merkle_proof" "$in_rollup_merkle_proof" "$in_global_index" "$in_main_exit_root" "$in_rollup_exit_root" "$in_orig_net" "$in_orig_addr" "$in_dest_net" "$in_dest_addr" "$in_amount" "$in_metadata"
+  # Get our variables organized
+  in_merkle_proof="$(jq -r -c '.proof.merkle_proof' proof.json | tr -d '"')"
+  in_rollup_merkle_proof="$(jq -r -c '.proof.rollup_merkle_proof' proof.json | tr -d '"')"
+  in_global_index="$(echo $tx | jq -r '.global_index')"
+  in_main_exit_root="$(jq -r '.proof.main_exit_root' proof.json)"
+  in_rollup_exit_root="$(jq -r '.proof.rollup_exit_root' proof.json)"
+  in_orig_net="$(echo $tx | jq -r '.orig_net')"
+  in_orig_addr="$(echo $tx | jq -r '.orig_addr')"
+  in_dest_net="$(echo $tx | jq -r '.dest_net')"
+  in_dest_addr="$(echo $tx | jq -r '.dest_addr')"
+  in_amount="$(echo $tx | jq -r '.amount')"
+  in_metadata="$(echo $tx | jq -r '.metadata')"
 
-# Perform an eth_call to make sure the tx will work
-echo "Performing an eth call to make sure the bridge claim tx will work..."
-cast call --rpc-url "$l1_rpc_url" "$bridge_addr" "$claim_sig" "$in_merkle_proof" "$in_rollup_merkle_proof" "$in_global_index" "$in_main_exit_root" "$in_rollup_exit_root" "$in_orig_net" "$in_orig_addr" "$in_dest_net" "$in_dest_addr" "$in_amount" "$in_metadata"
+  # Generate the call data, this is useful just to examine what the call will look loke
+  echo "Generating the call data for the bridge claim tx..."
+  cast calldata "$claim_sig" "$in_merkle_proof" "$in_rollup_merkle_proof" "$in_global_index" "$in_main_exit_root" "$in_rollup_exit_root" "$in_orig_net" "$in_orig_addr" "$in_dest_net" "$in_dest_addr" "$in_amount" "$in_metadata"
 
-# Publish the actual transaction!
-echo "Publishing the bridge claim tx..."
-cast send --rpc-url "$l1_rpc_url" --private-key "$private_key" "$bridge_addr" "$claim_sig" "$in_merkle_proof" "$in_rollup_merkle_proof" "$in_global_index" "$in_main_exit_root" "$in_rollup_exit_root" "$in_orig_net" "$in_orig_addr" "$in_dest_net" "$in_dest_addr" "$in_amount" "$in_metadata"
+  # Perform an eth_call to make sure the tx will work
+  echo "Performing an eth call to make sure the bridge claim tx will work..."
+  cast call --rpc-url "$l1_rpc_url" "$bridge_addr" "$claim_sig" "$in_merkle_proof" "$in_rollup_merkle_proof" "$in_global_index" "$in_main_exit_root" "$in_rollup_exit_root" "$in_orig_net" "$in_orig_addr" "$in_dest_net" "$in_dest_addr" "$in_amount" "$in_metadata"
+
+  # Publish the actual transaction!
+  echo "Publishing the bridge claim tx..."
+  cast send --rpc-url "$l1_rpc_url" --private-key "$private_key" "$bridge_addr" "$claim_sig" "$in_merkle_proof" "$in_rollup_merkle_proof" "$in_global_index" "$in_main_exit_root" "$in_rollup_exit_root" "$in_orig_net" "$in_orig_addr" "$in_dest_net" "$in_dest_addr" "$in_amount" "$in_metadata"
+done
