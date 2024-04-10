994e4ddde0a0b65efe7a47522932be90bffe4d95
leovct <l009.vincent@gmail.com>
2024-04-10 09:35:07 +0200

    chore: update tool check script

--- scripts/tool_check.sh
+++ scripts/tool_check.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
-set -e
 
 # Define minimum versions required to run the Kurtosis CDK packages.
 MINIMUM_KURTOSIS_VERSION_REQUIRED=0.88.9
 MINIMUM_DOCKER_VERSION_REQUIRED=24.7
 MAXIMUM_YQ_MAJOR_VERSION_SUPPORTED=3.x
 
-ensure_tool_installed() {
-    tool="$1"
-    install_docs="$2"
-    if ! command -v "$tool" &> /dev/null; then
+## Helper functions.
+ensure_required_tool_is_installed() {
+    local tool="$1"
+    local install_docs="$2"
+    if ! command -v "$tool" &>/dev/null; then
         echo "❌ $tool is not installed. Please install $tool to proceed: $install_docs"
+        exit 1
+    fi
+}
+
+ensure_optional_tool_is_installed() {
+    local tool="$1"
+    local install_docs="$2"
+    if ! command -v "$tool" &>/dev/null; then
+        echo "⚠️  $tool is not installed. You can install $tool at: $install_docs"
         return 1
     fi
 }
 
+## Check tool versions.
 check_kurtosis_version() {
-    ensure_tool_installed "kurtosis" "https://docs.kurtosis.com/install/"
+    ensure_required_tool_is_installed "kurtosis" "https://docs.kurtosis.com/install/"
 
     minimum_major_kurtosis_version_required="$(echo "$MINIMUM_KURTOSIS_VERSION_REQUIRED" | cut -d '.' -f 1)"
     minimum_minor_kurtosis_version_required="$(echo "$MINIMUM_KURTOSIS_VERSION_REQUIRED" | cut -d '.' -f 2)"
@@ -41,7 +51,7 @@ check_kurtosis_version() {
 }
 
 check_docker_version() {
-    ensure_tool_installed "docker" "https://docs.docker.com/engine/install/"
+    ensure_required_tool_is_installed "docker" "https://docs.docker.com/engine/install/"
 
     minimum_major_docker_version_required="$(echo "$MINIMUM_DOCKER_VERSION_REQUIRED" | cut -d '.' -f 1)"
     minimum_minor_docker_version_required="$(echo "$MINIMUM_DOCKER_VERSION_REQUIRED" | cut -d '.' -f 2)"
@@ -61,73 +71,85 @@ check_docker_version() {
     fi
 }
 
-check_jq_version() {
-    ensure_tool_installed "jq" "https://jqlang.github.io/jq/download/"
-
-    jq_version="$(jq --version | cut -d '-' -f 2)"
-    echo "✅ jq $jq_version is installed"
-}
-
-check_yq_version() {
-    ensure_tool_installed "yq" "https://pypi.org/project/yq/"
-
-    maximum_yq_major_version_supported="$(echo "$MAXIMUM_YQ_MAJOR_VERSION_SUPPORTED" | cut -d '.' -f 1)"
-
-    yq_version="$(yq --version | cut -d ' ' -f 2)"
-    major_yq_version="$(echo "$yq_version" | cut -d '.' -f 1)"
-    if [ "$major_yq_version" -le "$maximum_yq_major_version_supported" ]; then
-        echo "✅ yq $yq_version is installed, meets the requirement (<=$MAXIMUM_YQ_MAJOR_VERSION_SUPPORTED)"
-    else
-        echo "❌ yq $yq_version is installed, but version $MAXIMUM_YQ_MAJOR_VERSION_SUPPORTED or higher is not supported"
-        exit 1
-    fi
-}
-
-check_cast_version() {
-    ensure_tool_installed "cast" "https://book.getfoundry.sh/getting-started/installation#using-foundryup"
-
-    cast_version="$(cast --version | cut -d ' ' -f 2)"
-    echo "✅ cast $cast_version is installed"
-}
-
-check_polycli_version() {
-    ensure_tool_installed "polycli" "https://github.com/maticnetwork/polygon-cli/releases"
-
-    polycli_version="$(polycli version | cut -d ' ' -f 4)"
-    echo "✅ polycli $polycli_version is installed"
-}
-
 check_docker_mac_connect_version() {
-    ensure_tool_installed "docker-mac-net-connect" "https://github.com/chipmk/docker-mac-net-connect?tab=readme-ov-file#installation"
+    ensure_required_tool_is_installed "docker-mac-net-connect" "https://github.com/chipmk/docker-mac-net-connect?tab=readme-ov-file#installation"
 
     docker_mac_connect_version="$(docker-mac-net-connect --version | cut -d ' ' -f 3 | cut -d "'" -f 2 | cut -d "v" -f 2)"
     echo "✅ docker-mac-connect $docker_mac_connect_version is installed"
 }
 
-
-echo "Ensuring all required tools and versions are installed for deploying the Kurtosis CDK package..."
-check_kurtosis_version
-check_docker_version
-check_jq_version
-check_yq_version
-check_cast_version
-check_polycli_version
-
-if [[ "$(uname)" == "Darwin" ]]; then
-    echo; echo "Checking macOS specific tools..."
-    check_docker_mac_connect_version
-
-    echo; echo "Running a dummy nginx container..."
-    if docker ps -a --format '{{.Names}}' | grep -q '^nginx$'; then
-        docker rm -f nginx
+check_jq_version() {
+    ensure_optional_tool_is_installed "jq" "https://jqlang.github.io/jq/download/"
+    if [ $? -eq 0 ]; then
+        jq_version="$(jq --version | cut -d '-' -f 2)"
+        echo "✅ jq $jq_version is installed"
     fi
-    docker run --rm --name nginx -d nginx
+}
 
-    echo; echo "Making an HTTP request directly to the internal IP of the nginx container..."
-    if ! curl -m 1 -I "$(docker inspect nginx --format '{{.NetworkSettings.IPAddress}}')"; then
-        echo "Curl request failed. Make sure docker-mac-connect is running and make sure you reinstalled Docker Engine for macOS!"
-        exit 1
+check_yq_version() {
+    ensure_optional_tool_is_installed "yq" "https://pypi.org/project/yq/"
+    if [ $? -eq 0 ]; then
+        maximum_yq_major_version_supported="$(echo "$MAXIMUM_YQ_MAJOR_VERSION_SUPPORTED" | cut -d '.' -f 1)"
+
+        yq_version="$(yq --version | cut -d ' ' -f 2)"
+        major_yq_version="$(echo "$yq_version" | cut -d '.' -f 1)"
+        if [ "$major_yq_version" -le "$maximum_yq_major_version_supported" ]; then
+            echo "✅ yq $yq_version is installed, meets the requirement (<=$MAXIMUM_YQ_MAJOR_VERSION_SUPPORTED)"
+        else
+            echo "❌ yq $yq_version is installed, but version $MAXIMUM_YQ_MAJOR_VERSION_SUPPORTED or higher is not supported"
+            exit 1
+        fi
     fi
-fi
+}
 
-echo "✅ You are ready to go!"
+check_cast_version() {
+    ensure_optional_tool_is_installed "cast" "https://book.getfoundry.sh/getting-started/installation#using-foundryup"
+    if [ $? -eq 0 ]; then
+        cast_version="$(cast --version | cut -d ' ' -f 2)"
+        echo "✅ cast $cast_version is installed"
+    fi
+}
+
+check_polycli_version() {
+    ensure_optional_tool_is_installed "polycli" "https://github.com/maticnetwork/polygon-cli/releases"
+    if [ $? -eq 0 ]; then
+        polycli_version="$(polycli version | cut -d ' ' -f 4)"
+        echo "✅ polycli $polycli_version is installed"
+    fi
+}
+
+## Main function.
+main() {
+    echo "Checking that you have the necessary tools to deploy the Kurtosis CDK package..."
+    check_kurtosis_version
+    check_docker_version
+    if [[ "$(uname)" == "Darwin" ]]; then
+        echo; echo "Checking macOS specific tools..."
+        check_docker_mac_connect_version
+
+        echo; echo "Running a dummy nginx container..."
+        if docker ps -a --format '{{.Names}}' | grep -q '^nginx$'; then
+            docker rm -f nginx 2>&1 > /dev/null
+        fi
+        docker run --rm --name nginx -d nginx
+
+        echo; echo "Making an HTTP request directly to the internal IP of the nginx container..."
+        if ! curl -m 1 -I "$(docker inspect nginx --format '{{.NetworkSettings.IPAddress}}')"; then
+            echo "❌ Curl request failed. Make sure docker-mac-connect is running and make sure you reinstalled Docker Engine for macOS!"
+            exit 1
+        else
+            echo "✅ You can successfully access containers using their internal IPs!"
+            docker rm -f nginx 2>&1 > /dev/null
+        fi
+    fi
+
+    echo; echo "You might as well need the following tools to interact with the environment..."
+    check_jq_version
+    check_yq_version
+    check_cast_version
+    check_polycli_version
+
+    echo; echo "✅ You are ready to go!"
+}
+
+main
