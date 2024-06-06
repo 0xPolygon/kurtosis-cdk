22d6a4801188e6264ea4aa985a16dc1fd8324f16
leovct <l009.vincent@gmail.com>
2024-06-06 12:40:16 +0200

    fix: shellcheck issues

--- scripts/cdk-erigon/set-local-config.sh
+++ scripts/cdk-erigon/set-local-config.sh
@@ -14,16 +14,21 @@ contract_addresses="$(kurtosis service exec cdk-v1 contracts-001 "cat /opt/zkevm
 
 # Set the zkevm contract addresses in params.yml
 zkevm_rollup_address="$(echo "$contract_addresses" | jq --raw-output .rollupAddress)"
+# shellcheck disable=SC2016
 yq -Y --in-place --arg a "$zkevm_rollup_address" '.args.zkevm_rollup_address = $a' params.yml
 
 zkevm_rollup_manager_address="$(echo "$contract_addresses" | jq --raw-output .polygonRollupManagerAddress)"
+# shellcheck disable=SC2016
 yq -Y --in-place --arg a "$zkevm_rollup_manager_address" '.args.zkevm_rollup_manager_address = $a' params.yml
 
 zkevm_global_exit_root_address="$(echo "$contract_addresses" | jq --raw-output .polygonZkEVMGlobalExitRootAddress)"
+# shellcheck disable=SC2016
 yq -Y --in-place --arg a "$zkevm_global_exit_root_address" '.args.zkevm_global_exit_root_address = $a' params.yml
 
 pol_token_address="$(echo "$contract_addresses" | jq --raw-output .polTokenAddress)"
+# shellcheck disable=SC2016
 yq -Y --in-place --arg a "$pol_token_address" '.args.pol_token_address = $a' params.yml
 
 zkevm_rollup_manager_block_number="$(echo "$contract_addresses" | jq --raw-output .deploymentRollupManagerBlockNumber)"
+# shellcheck disable=SC2016
 yq -Y --in-place --arg a "$zkevm_rollup_manager_block_number" '.args.zkevm_rollup_manager_block_number = $a' params.yml
