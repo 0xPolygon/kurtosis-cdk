59df06324477ee799e8da0d278250c7ed30fb947
dan moore <dmoore@polygon.technology>
2024-07-05 10:29:54 -0500

    refine db reset cmds and permissions

--- scripts/reset_remote_postgres.sh
+++ scripts/reset_remote_postgres.sh
@@ -13,20 +13,31 @@ for i in "${!DB_NAMES[@]}"; do
 
     echo "Resetting database: $DB_NAME"
 
+    # initially connect as master postgres user to drop/recreate dbs
     PGPASSWORD=$PGPASSWORD psql -h $HOST -p $PORT -U postgres <<EOF
     DROP DATABASE IF EXISTS $DB_NAME;
     CREATE DATABASE $DB_NAME;
-    GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
+    GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;   
 EOF
 
+    # connect to specific database for db initialization
+    PGPASSWORD=$PGPASSWORD psql -h $HOST -p $PORT -U postgres -d $DB_NAME <<EOF
+    CREATE SCHEMA IF NOT EXISTS public;
+    GRANT USAGE ON SCHEMA public TO $DB_USER;
+    GRANT CREATE ON SCHEMA public TO $DB_USER;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO $DB_USER;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT EXECUTE ON FUNCTIONS TO $DB_USER;
+    GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO $DB_USER;
+    GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO $DB_USER;  
+EOF
     echo "Database $DB_NAME has been reset and permissions granted to $DB_USER."
 
     if [ "$DB_NAME" == "event_db" ]; then
-        echo "Setting up schema for $DB_NAME"
+        echo "Setting up public.event table for $DB_NAME"
         PGPASSWORD=$PGPASSWORD psql -h $HOST -p $PORT -U postgres -d $DB_NAME <<EOF
-        CREATE TYPE level_t AS ENUM ('emerg', 'alert', 'crit', 'err', 'warning', 'notice', 'info', 'debug');
+        CREATE TYPE IF NOT EXISTS level_t AS ENUM ('emerg', 'alert', 'crit', 'err', 'warning', 'notice', 'info', 'debug');
 
-        CREATE TABLE public.event (
+        CREATE TABLE IF NOT EXISTS public.event (
            id BIGSERIAL PRIMARY KEY,
            received_at timestamp WITH TIME ZONE default CURRENT_TIMESTAMP,
            ip_address inet,
@@ -42,16 +53,22 @@ EOF
     fi
 
     if [ "$DB_NAME" == "prover_db" ]; then
-        echo "Setting up schema for $DB_NAME"
+        echo "Setting up schema state for $DB_NAME"
         PGPASSWORD=$PGPASSWORD psql -h $HOST -p $PORT -U postgres -d $DB_NAME <<EOF
-        CREATE SCHEMA state;
+        CREATE SCHEMA IF NOT EXISTS state;
+        GRANT USAGE ON SCHEMA state TO $DB_USER;
+        GRANT CREATE ON SCHEMA state TO $DB_USER;
+        ALTER DEFAULT PRIVILEGES IN SCHEMA state GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO $DB_USER;
+        ALTER DEFAULT PRIVILEGES IN SCHEMA state GRANT EXECUTE ON FUNCTIONS TO $DB_USER;
+        GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA state TO $DB_USER;
+        GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA state TO $DB_USER;
 
-        CREATE TABLE state.nodes (
+        CREATE TABLE IF NOT EXISTS state.nodes (
            hash BYTEA PRIMARY KEY,
            data BYTEA NOT NULL
         );
 
-        CREATE TABLE state.program (
+        CREATE TABLE IF NOT EXISTS state.program (
            hash BYTEA PRIMARY KEY,
            data BYTEA NOT NULL
         );
