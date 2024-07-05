a03d81ffd861beaa65ef0cf485f1942658892ddf
dan moore <dmoore@polygon.technology>
2024-07-05 12:59:50 -0500

    cleanup db reset script

--- scripts/reset_remote_postgres.sh
+++ scripts/reset_remote_postgres.sh
@@ -3,9 +3,10 @@
 DB_NAMES=("event_db" "pool_db" "prover_db" "state_db" "agglayer_db" "bridge_db" "dac_db")
 DB_USERS=("event_user" "pool_user" "prover_user" "state_user" "agglayer_user" "bridge_user" "dac_user")
 
-PGPASSWORD='postgres'
-HOST='your_server_ip'
-PORT=5432
+PGPASSWORD='postgres'   # adjust credentials per your use case
+PGUSER='postgres'
+PGHOST='your_server_ip'
+PGPORT=5432
 
 for i in "${!DB_NAMES[@]}"; do
     DB_NAME="${DB_NAMES[$i]}"
@@ -14,14 +15,14 @@ for i in "${!DB_NAMES[@]}"; do
     echo "Resetting database: $DB_NAME"
 
     # initially connect as master postgres user to drop/recreate dbs
-    PGPASSWORD=$PGPASSWORD psql -h $HOST -p $PORT -U postgres <<EOF
+    PGPASSWORD=$PGPASSWORD psql -h $PGHOST -p $PGPORT -U $PGUSER <<EOF
     DROP DATABASE IF EXISTS $DB_NAME;
     CREATE DATABASE $DB_NAME;
     GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;   
 EOF
 
-    # connect to specific database for db initialization
-    PGPASSWORD=$PGPASSWORD psql -h $HOST -p $PORT -U postgres -d $DB_NAME <<EOF
+    # connect to specific database for db initialization                                                                                                                                                                                                                                                                                                                  
+    PGPASSWORD=$PGPASSWORD psql -h $PGHOST -p $PGPORT -U $PGUSER -d $DB_NAME <<EOF
     CREATE SCHEMA IF NOT EXISTS public;
     GRANT USAGE ON SCHEMA public TO $DB_USER;
     GRANT CREATE ON SCHEMA public TO $DB_USER;
@@ -30,11 +31,11 @@ EOF
     GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO $DB_USER;
     GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO $DB_USER;  
 EOF
-    echo "Database $DB_NAME has been reset and permissions granted to $DB_USER."
+    echo "Database $DB_NAME reset, permissions granted for user $DB_USER"
 
     if [ "$DB_NAME" == "event_db" ]; then
-        echo "Setting up public.event table for $DB_NAME"
-        PGPASSWORD=$PGPASSWORD psql -h $HOST -p $PORT -U postgres -d $DB_NAME <<EOF
+        echo "Setting up 'public.event' table for $DB_NAME"
+        PGPASSWORD=$PGPASSWORD psql -h $PGHOST -p $PGPORT -U $PGUSER -d $DB_NAME <<EOF
         CREATE TYPE IF NOT EXISTS level_t AS ENUM ('emerg', 'alert', 'crit', 'err', 'warning', 'notice', 'info', 'debug');
 
         CREATE TABLE IF NOT EXISTS public.event (
@@ -53,8 +54,8 @@ EOF
     fi
 
     if [ "$DB_NAME" == "prover_db" ]; then
-        echo "Setting up schema state for $DB_NAME"
-        PGPASSWORD=$PGPASSWORD psql -h $HOST -p $PORT -U postgres -d $DB_NAME <<EOF
+        echo "Setting up schema 'state' for $DB_NAME"
+        PGPASSWORD=$PGPASSWORD psql -h $PGHOST  -p $PGPORT -U $PGUSER -d $DB_NAME <<EOF
         CREATE SCHEMA IF NOT EXISTS state;
         GRANT USAGE ON SCHEMA state TO $DB_USER;
         GRANT CREATE ON SCHEMA state TO $DB_USER;
