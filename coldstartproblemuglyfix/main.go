4475df157d25c1cd869a386594305e629621842a
bros <arnaubennassar5@gmail.com>
2024-08-16 18:35:29 +0000

    wip

--- coldstartproblemuglyfix/main.go
+++ coldstartproblemuglyfix/main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"math/big"
-	"time"
 
-	"github.com/0xPolygon/cdk-contracts-tooling/contracts/banana/polygonzkevmbridgev2"
+	"github.com/0xPolygon/cdk-contracts-tooling/contracts/banana/polygonzkevmglobalexitrootv2"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
@@ -17,44 +13,33 @@ const (
 	pk         = "12d7de8621a77640c9241b2595ba78ce443d05e94090365ab3bb5e19df82c625"
 	l1ChainID  = 271828
 	bridgeAddr = "0xD71f8F956AD979Cc2988381B8A743a2fE280537D"
-	l1RPC      = "http://localhost:32965"
+	gerAddr    = "0x1f7ad7caA53e35b4f0D138dC5CBF91aC108a2674"
+	l1RPC      = "http://localhost:33297"
 )
 
 func main() {
-	privateKey, err := crypto.HexToECDSA(pk)
-	if err != nil {
-		panic(err)
-	}
-	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, big.NewInt(l1ChainID))
-	if err != nil {
-		panic(err)
-	}
+	// privateKey, err := crypto.HexToECDSA(pk)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// auth, err := bind.NewKeyedTransactorWithChainID(privateKey, big.NewInt(l1ChainID))
+	// if err != nil {
+	// 	panic(err)
+	// }
 	client, err := ethclient.Dial(l1RPC)
 	if err != nil {
 		panic(err)
 	}
-	contract, err := polygonzkevmbridgev2.NewPolygonzkevmbridgev2(
-		common.HexToAddress(bridgeAddr),
+	contract, err := polygonzkevmglobalexitrootv2.NewPolygonzkevmglobalexitrootv2(
+		common.HexToAddress(gerAddr),
 		client,
 	)
 	if err != nil {
 		panic(err)
 	}
-	tx, err := contract.BridgeAsset(
-		auth, 1, common.Address{}, big.NewInt(0), common.Address{}, true, nil,
-	)
+	ger, err := contract.L1InfoRootMap(&bind.CallOpts{Pending: false}, 1)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("bridge tx sent, waiting for it to be mined...")
-	for {
-		time.Sleep(time.Second)
-		r, err := client.TransactionReceipt(context.Background(), tx.Hash())
-		if err != nil {
-			fmt.Println("error getting tx receipt: ", err)
-			continue
-		}
-		fmt.Printf("tx receipt: %+v\n", r)
-		break
-	}
+	fmt.Println(common.Bytes2Hex(ger[:]))
 }
