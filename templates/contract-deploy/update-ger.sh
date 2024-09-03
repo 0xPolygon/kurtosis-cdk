1175efa9d8a7f81cfeb111217b8870941b4e16c8
leovct <l009.vincent@gmail.com>
2024-09-03 13:32:07 +0200

    chore: populate values using kurtosis templates

--- templates/contract-deploy/update-ger.sh
+++ templates/contract-deploy/update-ger.sh
@@ -3,14 +3,13 @@ set -e
 
 # Setup some vars for use later on
 # The private key used to send transactions
-private_key="0903a9a721167e2abaa0a33553cbeb209dc9300d28e4e4d6d2fac2452f93e357"
+private_key="{{.zkevm_l2_admin_private_key}}"
 
 # The bridge address
-bridge_addr="0xD71f8F956AD979Cc2988381B8A743a2fE280537D" # TODO: populate this addr using kurtosis magic
+bridge_address="$(jq --raw-output '.polygonZkEVMBridgeAddress' /opt/zkevm/combined.json)"
 
 # Grab the endpoints for l1
 l1_rpc_url="{{.l1_rpc_url}}"
-# l1_rpc_url="http://localhost:33201"
 
 # The signature for bridging is long - just putting it into a var
 bridge_sig="bridgeAsset(uint32 destinationNetwork, address destinationAddress, uint256 amount, address token, bool forceUpdateGlobalExitRoot, bytes permitData)"
@@ -29,8 +28,8 @@ cast calldata "$bridge_sig" "$destination_net" "$destination_addr" "$amount" "$t
 
 # Perform an eth_call to make sure the tx will work
 echo "Performing an eth call to make sure the bridge tx will work..."
-cast call --rpc-url "$l1_rpc_url" "$bridge_addr" "$bridge_sig" "$destination_net" "$destination_addr" "$amount" "$token" "$update_ger" "$permit_data"
+cast call --rpc-url "$l1_rpc_url" "$bridge_address" "$bridge_sig" "$destination_net" "$destination_addr" "$amount" "$token" "$update_ger" "$permit_data"
 
 # Publish the actual transaction!
 echo "Publishing the bridge tx..."
-cast send --rpc-url "$l1_rpc_url" --private-key "$private_key" "$bridge_addr" "$bridge_sig" "$destination_net" "$destination_addr" "$amount" "$token" "$update_ger" "$permit_data"
+cast send --rpc-url "$l1_rpc_url" --private-key "$private_key" "$bridge_address" "$bridge_sig" "$destination_net" "$destination_addr" "$amount" "$token" "$update_ger" "$permit_data"
