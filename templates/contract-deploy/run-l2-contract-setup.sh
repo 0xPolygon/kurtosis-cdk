a841ff9b2c6fc1cbca3246923e2d710203b6942d
minhd-vu <minhd_vu@yahoo.com>
2024-10-21 20:05:34 -0400

    deploy ddp

--- templates/contract-deploy/run-l2-contract-setup.sh
+++ templates/contract-deploy/run-l2-contract-setup.sh
@@ -9,7 +9,7 @@ echo_ts() {
     local green="\e[32m"
     local end_color="\e[0m"
     local timestamp
-    timestamp=$(date + "[%Y-%m-%d %H:%M:%S]")
+    timestamp=$(date +"[%Y-%m-%d %H:%M:%S]")
 
     echo -e "$green$timestamp$end_color $1" >&2
 }
@@ -65,6 +65,85 @@ accounts=$(
 echo "$accounts" | jq -r '.Addresses[].ETHAddress' | while read -r address; do
     fund_account_on_l2 "$address"
 done
+
+echo_ts "Building deterministic deployment proxy"
+git clone https://github.com/Arachnid/deterministic-deployment-proxy.git \
+    /opt/deterministic-deployment-proxy \
+    --branch "{{.deterministic_deployment_proxy_branch}}"
+cd /opt/deterministic-deployment-proxy
+npm ci
+npm run build
+
+signer_address="0x$(cat output/deployment.json | jq --raw-output '.signerAddress')"
+gas_price=$(cat output/deployment.json | jq --raw-output '.gasPrice')
+gas_limit=$(cat output/deployment.json | jq --raw-output '.gasLimit')
+gas_cost=$((gas_price * gas_limit))
+transaction="0x$(cat output/deployment.json | jq --raw-output '.transaction')"
+bytecode=$(cat output/bytecode.txt)
+deployer_address="0x$(cat output/deployment.json | jq --raw-output '.address')"
+contract_method_signature="0xc3cafc6f"
+expected="0x000000000000000000000000000000000000000000000000000000000000002a"
+
+echo_ts "Deploying deterministic deployment proxy on l2"
+cast send \
+    --legacy \
+    --rpc-url "{{.l2_rpc_url}}" \
+    --private-key "{{.zkevm_l2_admin_private_key}}" \
+    --value "$gas_cost" \
+    "$signer_address"
+cast publish --rpc-url "{{.l2_rpc_url}}" "$transaction"
+
+echo_ts "Deploying deterministic deployment proxy on l1"
+cast send \
+    --rpc-url "{{.l1_rpc_url}}" \
+    --mnemonic "{{.l1_preallocated_mnemonic}}" \
+    --value "$gas_cost" \
+    "$signer_address"
+cast publish --rpc-url "{{.l1_rpc_url}}" "$transaction"
+
+echo_ts "Testing deterministic deployment proxy on l2"
+cast send \
+    --legacy \
+    --rpc-url "{{.l2_rpc_url}}" \
+    --private-key "{{.zkevm_l2_admin_private_key}}" \
+    --create "0x0000000000000000000000000000000000000000000000000000000000000000$bytecode" \
+    "$deployer_address"
+l2_contract_address=$(
+    cast call \
+        --rpc-url "{{.l2_rpc_url}}" \
+        --private-key "{{.zkevm_l2_admin_private_key}}" \
+        "$deployer_address" \
+        "0x0000000000000000000000000000000000000000000000000000000000000000$bytecode"
+)
+l2_actual=$(cast call --rpc-url "{{.l2_rpc_url}}" "$l2_contract_address" "$contract_method_signature")
+if [ "$expected" != "$l2_actual" ]; then
+  echo "Failed to deploy deterministic deployment proxy on l2"
+  exit 1
+fi
+
+echo_ts "Testing deterministic deployment proxy on l1"
+cast send \
+    --rpc-url "{{.l1_rpc_url}}" \
+    --mnemonic "{{.l1_preallocated_mnemonic}}" \
+    --create "0x0000000000000000000000000000000000000000000000000000000000000000$bytecode" \
+    "$deployer_address"
+l1_contract_address=$(
+    cast call \
+        --rpc-url "{{.l1_rpc_url}}" \
+        --mnemonic "{{.l1_preallocated_mnemonic}}" \
+        "$deployer_address" \
+        "0x0000000000000000000000000000000000000000000000000000000000000000$bytecode"
+)
+l1_actual=$(cast call --rpc-url "{{.l1_rpc_url}}" "$l1_contract_address" "$contract_method_signature")
+if [ "$expected" != "$l1_actual" ]; then
+  echo "Failed to deploy deterministic deployment proxy on l1"
+  exit 1
+fi
+
+if [ "$l1_contract_address" != "$l2_contract_address" ]; then
+  echo "Mismatch contract addresses deployed by deterministic deployment proxy"
+  exit 1
+fi
 
 # The contract setup is done!
 touch "/opt/zkevm/.init-complete{{.deployment_suffix}}.lock"
