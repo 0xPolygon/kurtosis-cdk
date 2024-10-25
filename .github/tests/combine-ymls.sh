08e848cb7fb70cbd47897539ad56740a9fe713d2
John Hilliard <praetoriansentry@gmail.com>
2024-10-24 22:38:04 -0400

    chore: updating ymls

--- .github/tests/combine-ymls.sh
+++ .github/tests/combine-ymls.sh
@@ -19,18 +19,27 @@ mkdir -p "$COMBINATIONS_FOLDER"
 for fork in "${forks[@]}"; do
     for da in "${data_availability[@]}"; do
         for comp in "${components[@]}"; do
-            # Skipping tests for:
-            # - cdk-validium-node with fork 11 and fork 12, as they are not currently not supported.
-            # - zkevm-node with fork 12, as it is also not supported.
-            if [ "$(extract_base_name "$comp")" == "legacy-zkevm-stack" ] && {
-                {
-                    [ "$(extract_base_name "$fork")" == "fork11" ] && [ "$(extract_base_name "$da")" == "cdk-validium" ]
-                } || [ "$(extract_base_name "$fork")" == "fork12" ];
-            }; then
+            base_fork="$(extract_base_name "$fork")"
+            base_da="$(extract_base_name "$da")"
+            base_comp="$(extract_base_name "$comp")"
+
+
+            # The legacy stack doesn't work with fork 12
+            if [[ "$base_fork" == "fork12" && "$base_comp" == "legacy-zkevm-stack" ]]; then
                 continue
             fi
 
-            output_file="$COMBINATIONS_FOLDER/$(extract_base_name "$fork")-$(extract_base_name "$comp")-$(extract_base_name "$da").yml"
+            # The legacy stack also doesn't work with fork 13
+            if [[ "$base_fork" == "fork13" && "$base_comp" == "legacy-zkevm-stack" ]]; then
+                continue
+            fi
+
+            # The combination of fork 11 with the zkevm stack with validium mode does not work
+            if [[ "$base_fork" == "fork11" && "$base_comp" == "legacy-zkevm-stack" && "$base_da" == "cdk-validium" ]]; then
+                continue
+            fi
+
+            output_file="$COMBINATIONS_FOLDER/$base_fork-$base_comp-$base_da.yml"
             yq --slurp ".[0] * .[1] * .[2]" "$fork" "$da" "$comp" --yaml-output > "$output_file"
             echo "- $output_file"
         done
