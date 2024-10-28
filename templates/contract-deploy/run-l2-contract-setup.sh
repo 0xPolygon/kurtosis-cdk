eda8a528669ffc1bd25bfcf279109b8273af2153
minhd-vu <minhd_vu@yahoo.com>
2024-10-28 11:35:25 -0400

    fix private key

--- templates/contract-deploy/run-l2-contract-setup.sh
+++ templates/contract-deploy/run-l2-contract-setup.sh
@@ -75,7 +75,7 @@ signer_address="0x3fab184622dc19b6109349b94811493bf2a45362"
 gas_cost="0.01ether"
 transaction="0xf8a58085174876e800830186a08080b853604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf31ba02222222222222222222222222222222222222222222222222222222222222222a02222222222222222222222222222222222222222222222222222222222222222"
 deployer_address="0x4e59b44847b379578588920ca78fbf26c0b4956c"
-l1_private_key=$(polycli wallet inspect --mnemonic "{{.l1_preallocated_mnemonic}}" | jq -r ".Addresses[0].HexPrivateKey")
+l1_private_key=$(cast wallet private-key --mnemonic "{{.l1_preallocated_mnemonic}}")
 
 echo_ts "Deploying deterministic deployment proxy on l1"
 cast send \
