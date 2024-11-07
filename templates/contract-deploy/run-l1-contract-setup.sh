e05040165c7a5e783b25a8494d9c781cd40dff0d
leovct <l009.vincent@gmail.com>
2024-11-07 15:29:53 +0100

    docs: l1 contract steup

--- templates/contract-deploy/run-l1-contract-setup.sh
+++ templates/contract-deploy/run-l1-contract-setup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # This script is responsible for deploying the contracts for zkEVM/CDK.
-global_log_level="{{.global_log_level}}"
-if [[ $global_log_level == "debug" ]]; then
-    set -x
-fi
+
+# --------------------------------------------------------------------------------------------------
+#   _____ _   _ _   _  ____ _____ ___ ___  _   _ ____  
+#  |  ___| | | | \ | |/ ___|_   _|_ _/ _ \| \ | / ___| 
+#  | |_  | | | |  \| | |     | |  | | | | |  \| \___ \ 
+#  |  _| | |_| | |\  | |___  | |  | | |_| | |\  |___) |
+#  |_|    \___/|_| \_|\____| |_| |___\___/|_| \_|____/ 
+#
+# --------------------------------------------------------------------------------------------------
 
 echo_ts() {
     green="\e[32m"
@@ -41,10 +46,10 @@ wait_for_finalized_block() {
     done
 }
 
-fund_account_on_l1() {
+fund_address_on_l1() {
     name="$1"
     address="$2"
-    echo_ts "Funding $name account"
+    echo_ts "Funding $name address"
     cast send \
         --rpc-url "{{.l1_rpc_url}}" \
         --mnemonic "{{.l1_preallocated_mnemonic}}" \
@@ -52,14 +57,26 @@ fund_account_on_l1() {
         "$address"
 }
 
+deploy_gas_token_contract() {
+    echo_ts "Deploying gas token to L1"
+    forge create \
+        --json \
+        --rpc-url "{{.l1_rpc_url}}" \
+        --mnemonic "{{.l1_preallocated_mnemonic}}" \
+        contracts/mocks/ERC20PermitMock.sol:ERC20PermitMock \
+        --constructor-args  "CDK Gas Token" "CDK" "{{.zkevm_l2_admin_address}}" "1000000000000000000000000"
+}
+
 deploy_zkevm_contracts() {
+    echo_ts "Deploying the zkevm contracts..."
+
     echo_ts "Step 0: Preparing tesnet"
     npx hardhat run deployment/testnet/prepareTestnet.ts --network localhost 2>&1 | tee 01_prepare_testnet.out
 
     echo_ts "Step 1: Creating genesis"
     MNEMONIC="{{.l1_preallocated_mnemonic}}" npx ts-node deployment/v2/1_createGenesis.ts 2>&1 | tee 02_create_genesis.out
     if [[ ! -e deployment/v2/genesis.json ]]; then
-        echo_ts "The genesis file was not created after running createGenesis"
+        echo_ts "The genesis file was not created after running createGenesis."
         exit 1
     fi
 
@@ -69,13 +86,36 @@ deploy_zkevm_contracts() {
     echo_ts "Step 3: Deploying contracts"
     npx hardhat run deployment/v2/3_deployContracts.ts --network localhost 2>&1 | tee 04_deploy_contracts.out
     if [[ ! -e deployment/v2/deploy_output.json ]]; then
-        echo_ts "The deploy_output.json file was not created after running deployContracts"
+        echo_ts "The deploy_output.json file was not created after running deployContracts."
         exit 1
     fi
 }
 
+deploy_rollup_contract() {
+    echo_ts "Step 4: Creating Rollup/Validium"
+    npx hardhat run deployment/v2/4_createRollup.ts --network localhost 2>&1 | tee 05_create_rollup.out
+    if [[ ! -e deployment/v2/create_rollup_output.json ]]; then
+        echo_ts "The create_rollup_output.json file was not created after running createRollup."
+        exit 1
+    fi
+}
+
+# --------------------------------------------------------------------------------------------------
+#   __  __    _    ___ _   _ 
+#  |  \/  |  / \  |_ _| \ | |
+#  | |\/| | / _ \  | ||  \| |
+#  | |  | |/ ___ \ | || |\  |
+#  |_|  |_/_/   \_\___|_| \_|
+#
+# --------------------------------------------------------------------------------------------------
+
+global_log_level="{{.global_log_level}}"
+if [[ $global_log_level == "debug" ]]; then
+    set -x
+fi
+
 if [[ -e "/opt/zkevm/.init-complete{{.deployment_suffix}}.lock" ]]; then
-    echo_ts "This script has already been executed"
+    echo_ts "Skipping. This script has already been executed."
     exit 1
 fi
 
@@ -83,63 +123,67 @@ fi
 # kurtosis entirely, we'll use those and exit. This is like a
 # permissionless use case
 if [[ -e "/opt/contract-deploy/genesis.json" && -e "/opt/contract-deploy/combined.json" ]]; then
-    echo_ts "We have a genesis and combined output file from a previous deployment"
+    echo_ts "Skipping. We have a genesis and combined output file from a previous deployment."
     cp /opt/contract-deploy/* /opt/zkevm/
-    exit
+    exit 1
 fi
 
-echo_ts "Waiting for the L1 RPC to be available"
+echo_ts "Waiting for the L1 RPC to be available..."
 wait_for_rpc_to_be_available "{{.l1_rpc_url}}"
-echo_ts "L1 RPC is now available"
+echo_ts "The L1 RPC is now available!"
 
-echo_ts "Funding important accounts on l1"
-fund_account_on_l1 "admin" "{{.zkevm_l2_admin_address}}"
-fund_account_on_l1 "sequencer" "{{.zkevm_l2_sequencer_address}}"
-fund_account_on_l1 "aggregator" "{{.zkevm_l2_aggregator_address}}"
-fund_account_on_l1 "agglayer" "{{.zkevm_l2_agglayer_address}}"
-fund_account_on_l1 "l1testing" "{{.l1_deposit_account}}"
+# --------------------------------------------------------------------------------------------------
+#    ____ ___  _   _ _____ ____      _    ____ _____   ____  _____ ____  _     _____   ____  __ _____ _   _ _____ 
+#   / ___/ _ \| \ | |_   _|  _ \    / \  / ___|_   _| |  _ \| ____|  _ \| |   / _ \ \ / /  \/  | ____| \ | |_   _|
+#  | |  | | | |  \| | | | | |_) |  / _ \| |     | |   | | | |  _| | |_) | |  | | | \ V /| |\/| |  _| |  \| | | |  
+#  | |__| |_| | |\  | | | |  _ <  / ___ \ |___  | |   | |_| | |___|  __/| |__| |_| || | | |  | | |___| |\  | | |  
+#   \____\___/|_| \_| |_| |_| \_\/_/   \_\____| |_|   |____/|_____|_|   |_____\___/ |_| |_|  |_|_____|_| \_| |_|  
+#
+# --------------------------------------------------------------------------------------------------
 
-echo_ts "Setting up local zkevm-contracts repo for deployment"
+echo_ts "Funding important addresses on L1..."
+fund_address_on_l1 "admin" "{{.zkevm_l2_admin_address}}"
+fund_address_on_l1 "sequencer" "{{.zkevm_l2_sequencer_address}}"
+fund_address_on_l1 "aggregator" "{{.zkevm_l2_aggregator_address}}"
+fund_address_on_l1 "agglayer" "{{.zkevm_l2_agglayer_address}}"
+fund_address_on_l1 "l1testing" "{{.l1_deposit_account}}"
+echo_ts "Addresses funded!"
+
+echo_ts "Setting up local zkevm-contracts repository for deployment..."
 pushd /opt/zkevm-contracts || exit 1
-# setup a foundry toml in case we do a gas token or dac deployment
-printf "[profile.default]\nsrc = 'contracts'\nout = 'out'\nlibs = ['node_modules']\n" > foundry.toml
-
 cp /opt/contract-deploy/deploy_parameters.json /opt/zkevm-contracts/deployment/v2/deploy_parameters.json
 cp /opt/contract-deploy/create_rollup_parameters.json /opt/zkevm-contracts/deployment/v2/create_rollup_parameters.json
 sed -i 's#http://127.0.0.1:8545#{{.l1_rpc_url}}#' hardhat.config.ts
+# Setup a foundry project in case we do a gas token or dac deployment.
+printf "[profile.default]\nsrc = 'contracts'\nout = 'out'\nlibs = ['node_modules']\n" > foundry.toml
 
-# Deploy gas token
-# TODO in the future this should be configurable. I.e. we should be able to specify a token address that has already been deployed
+# Deploy the gas token.
+# TODO: This should be configurable. We should be able to specify a token address that has already been deployed.
 # {{if .zkevm_use_gas_token_contract}}
-echo_ts "Deploying gas token to L1"
-forge create \
-    --json \
-    --rpc-url "{{.l1_rpc_url}}" \
-    --mnemonic "{{.l1_preallocated_mnemonic}}" \
-    contracts/mocks/ERC20PermitMock.sol:ERC20PermitMock \
-    --constructor-args  "CDK Gas Token" "CDK" "{{.zkevm_l2_admin_address}}" "1000000000000000000000000" > gasToken-erc20.json
-
-# In this case, we'll configure the create rollup parameters to have a gas token
+deploy_gas_token_contract > gasToken-erc20.json
 jq --slurpfile c gasToken-erc20.json '.gasTokenAddress = $c[0].deployedTo' /opt/contract-deploy/create_rollup_parameters.json > /opt/zkevm-contracts/deployment/v2/create_rollup_parameters.json
 # {{end}}
 
-is_first_rollup=0 # an indicator if this deployment is doing the first setup of the agglayer etc
+# Deploy the zkevm contracts.
+is_first_rollup=0 # An indicator if this deployment is doing the first setup of the agglayer.
 if [[ ! -e /opt/zkevm/combined.json ]]; then
-    echo_ts "It looks like this is the first rollup so we'll deploy the LxLy and Rollup Manager"
     deploy_zkevm_contracts
     is_first_rollup=1
 else
-    echo_ts "Skipping deployment of the Rollup Manager and LxLy"
+    echo_ts "Skipping the deployment of the zkevm-contracts."
     cp /opt/zkevm/genesis.json /opt/zkevm-contracts/deployment/v2/
     cp /opt/zkevm/deploy_output.json /opt/zkevm-contracts/deployment/v2/
 fi
+deploy_rollup_contract
 
-echo_ts "Step 4: Creating Rollup/Validium"
-npx hardhat run deployment/v2/4_createRollup.ts --network localhost 2>&1 | tee 05_create_rollup.out
-if [[ ! -e deployment/v2/create_rollup_output.json ]]; then
-    echo_ts "The create_rollup_output.json file was not created after running createRollup"
-    exit 1
-fi
+# --------------------------------------------------------------------------------------------------
+#    ____ ___  _   _ _____ ___ ____    ____ _____ _   _ _____ ____      _  _____ ___ ___  _   _ 
+#   / ___/ _ \| \ | |  ___|_ _/ ___|  / ___| ____| \ | | ____|  _ \    / \|_   _|_ _/ _ \| \ | |
+#  | |  | | | |  \| | |_   | | |  _  | |  _|  _| |  \| |  _| | |_) |  / _ \ | |  | | | | |  \| |
+#  | |__| |_| | |\  |  _|  | | |_| | | |_| | |___| |\  | |___|  _ <  / ___ \| |  | | |_| | |\  |
+#   \____\___/|_| \_|_|   |___\____|  \____|_____|_| \_|_____|_| \_\/_/   \_\_| |___\___/|_| \_|
+#
+# --------------------------------------------------------------------------------------------------
 
 # Combine contract deploy files.
 # At this point, all of the contracts /should/ have been deployed.
@@ -230,7 +274,14 @@ if [[ ! -s dynamic-kurtosis-conf.json ]]; then
     exit 1
 fi
 
-# Configure contracts.
+# --------------------------------------------------------------------------------------------------
+#    ____ ___  _   _ _____ ____      _    ____ _____    ____ ___  _   _ _____ ___ ____ _   _ ____      _  _____ ___ ___  _   _ 
+#   / ___/ _ \| \ | |_   _|  _ \    / \  / ___|_   _|  / ___/ _ \| \ | |  ___|_ _/ ___| | | |  _ \    / \|_   _|_ _/ _ \| \ | |
+#  | |  | | | |  \| | | | | |_) |  / _ \| |     | |   | |  | | | |  \| | |_   | | |  _| | | | |_) |  / _ \ | |  | | | | |  \| |
+#  | |__| |_| | |\  | | | |  _ <  / ___ \ |___  | |   | |__| |_| | |\  |  _|  | | |_| | |_| |  _ <  / ___ \| |  | | |_| | |\  |
+#   \____\___/|_| \_| |_| |_| \_\/_/   \_\____| |_|    \____\___/|_| \_|_|   |___\____|\___/|_| \_\/_/   \_\_| |___\___/|_| \_|
+#
+# --------------------------------------------------------------------------------------------------
 
 if [[ $is_first_rollup -eq 1 ]]; then
     # Grant the aggregator role to the agglayer so that it can also verify batches.
@@ -287,16 +338,13 @@ cast send \
     "$(jq -r '.polygonDataCommitteeAddress' combined.json)"
 # {{end}}
 
-# If we've configured the l1 network with the minimal preset, we
-# should probably wait for the first finalized block. This isn't
-# strictly specific to minimal preset, but if we don't have "minimal"
-# configured, it's going to take like 25 minutes for the first
-# finalized block
+# If we've configured the l1 network with the minimal preset, we should probably wait for the first
+# finalized block. This isn't strictly specific to minimal preset, but if we don't have "minimal"
+# configured, it's going to take like 25 minutes for the first finalized block.
 l1_preset="{{.l1_preset}}"
 if [[ $l1_preset == "minimal" ]]; then
-    wait_for_finalized_block;
+    wait_for_finalized_block
 fi
 
 # The contract setup is done!
 touch "/opt/zkevm/.init-complete{{.deployment_suffix}}.lock"
-
