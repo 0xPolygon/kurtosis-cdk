432a372940e8be44b50f2cac5c3a57f345cf98de
John Hilliard <praetoriansentry@gmail.com>
2024-12-02 23:17:39 -0500

    feat: latest changes for testing

--- multi-pp-test.sh
+++ multi-pp-test.sh
@@ -374,3 +374,157 @@ done
 # [agglayer] {"timestamp":"2024-11-27T20:18:52.021621Z","level":"WARN","fields":{"message":"Error during certification process of 0x24319d4ef5dda589b80498917cea4a66fd91494993fb391bfe5a02ddda84e674: native execution failed: MissingTokenBalanceProof(TokenInfo { origin_network: NetworkId(0), origin_token_address: 0x0000000000000000000000000000000000000000 })","hash":"0x24319d4ef5dda589b80498917cea4a66fd91494993fb391bfe5a02ddda84e674"},"target":"agglayer_certificate_orchestrator::network_task"}
 # [agglayer] {"timestamp":"2024-11-27T20:18:59.020532Z","level":"WARN","fields":{"message":"Certificate 0x24319d4ef5dda589b80498917cea4a66fd91494993fb391bfe5a02ddda84e674 is in error: (native) proof generation error: Missing token balance proof. TokenInfo: TokenInfo { origin_network: NetworkId(0), origin_token_address: 0x0000000000000000000000000000000000000000 }","hash":"0x24319d4ef5dda589b80498917cea4a66fd91494993fb391bfe5a02ddda84e674"},"target":"agglayer_certificate_orchestrator::network_task"}
 
+
+
+
+
+
+
+appleseed_address="0x56c069fd987CEA5A8e03E1c1AEB8089000f13bB6"
+
+cast send --legacy --rpc-url $l1_rpc_url --value 100000000000000000000 --private-key $private_key $appleseed_address
+# R0, R1, Native, Force
+cast send \
+    --legacy \
+    --rpc-url $l1_rpc_url \
+    --value 100000000000000000000 \
+    --private-key $private_key \
+    $bridge_address \
+    "bridgeAsset(uint32,address,uint256,address,bool,bytes)" \
+    1 $appleseed_address 100000000000000000000 $(cast az) true 0x
+
+# R0, R2, Native, Force
+cast send \
+    --legacy \
+    --rpc-url $l1_rpc_url \
+    --value 100000000000000000000 \
+    --private-key $private_key \
+    $bridge_address \
+    "bridgeAsset(uint32,address,uint256,address,bool,bytes)" \
+    2 $appleseed_address 100000000000000000000 $(cast az) true 0x
+
+function distribute() {
+
+    local mnemonic_index="$1"
+    local rpc_url="$2"
+    local lxly_bridge_addr="$3"
+    local cur_net="$4"
+
+    local dest_net
+
+    local seed_mnemonic="gallery option core excess loyal sing subject hold collect reason entry alpha"
+
+    local mnemonic_index_1=$((2 * mnemonic_index + 1))
+    local mnemonic_index_2=$((2 * mnemonic_index + 2))
+
+    local eth_address=$(cast wallet address --mnemonic "$seed_mnemonic" --mnemonic-index $mnemonic_index)
+    local eth_address_1=$(cast wallet address --mnemonic "$seed_mnemonic" --mnemonic-index $mnemonic_index_1)
+    local eth_address_2=$(cast wallet address --mnemonic "$seed_mnemonic" --mnemonic-index $mnemonic_index_2)
+
+    local balance
+    local min_balance_check
+    local lxly_bridge_addr
+
+    while true; do
+        balance=$(cast balance --rpc-url $rpc_url $eth_address)
+        min_balance_check=$(bc <<< "$balance > 1000000000") # should probably have at least 1gwei?
+        if [[ $min_balance_check -eq 1 ]]; then
+            break
+        fi
+        sleep 10
+    done
+
+    # do some work
+    # do some work
+    # do some work
+
+    if [[ $cur_net -eq 0 ]]; then
+        if [[ $(($RANDOM % 2)) -eq 0 ]]; then
+            dest_net=1
+        else
+            dest_net=2
+        fi
+    elif [[ $cur_net -eq 1 ]]; then
+        if [[ $(($RANDOM % 2)) -eq 0 ]]; then
+            dest_net=0
+        else
+            dest_net=2
+        fi
+    else
+        if [[ $(($RANDOM % 2)) -eq 0 ]]; then
+            dest_net=0
+        else
+            dest_net=1
+        fi
+    fi
+
+    local is_forced=true
+    if [[ $(($RANDOM % 2)) -eq 0 ]]; then
+        is_forced=false
+    fi
+
+    local bridge_data="0x"
+    if [[ $(($RANDOM % 2)) -eq 0 ]]; then
+        bridge_data="0x$(cat /dev/random | xxd -p | tr -d "\n" | head -c $((RANDOM & ~1)))"
+    fi
+
+
+    if [[ $(($RANDOM % 2)) -eq 0 ]]; then
+        set -x
+        cast send \
+             --legacy \
+             --rpc-url "$rpc_url" \
+             --value "$mnemonic_index" \
+             --gas-limit 1000000 \
+             --mnemonic "$seed_mnemonic" --mnemonic-index "$mnemonic_index" \
+             "$lxly_bridge_addr" \
+             "bridgeMessage(uint32,address,bool,bytes)" \
+             "$dest_net" "0x56c069fd987CEA5A8e03E1c1AEB8089000f13bB6" "$is_forced" "$bridge_data"
+        set +x
+    else
+        set -x
+        cast send \
+             --legacy \
+             --rpc-url "$rpc_url" \
+             --value "$mnemonic_index" \
+             --gas-limit 1000000 \
+             --mnemonic "$seed_mnemonic" --mnemonic-index "$mnemonic_index" \
+             "$lxly_bridge_addr" \
+             "bridgeAsset(uint32,address,uint256,address,bool,bytes)" \
+             "$dest_net" "0x56c069fd987CEA5A8e03E1c1AEB8089000f13bB6" "$mnemonic_index" "$(cast az)" "$is_forced" "$bridge_data"
+        set +x
+    fi
+
+    # finished
+    # finished
+    # finished
+
+    balance=$(cast balance --rpc-url $rpc_url $eth_address)
+
+    local gas_price="$(cast gas-price --rpc-url $rpc_url)"
+    gas_price=$(bc <<< "$gas_price * 3 / 2") # push the gas price up for faster mining
+
+    local leave_behind=271828
+    local dist_value=$(bc <<< "($balance - $leave_behind - (2 * 21000 * $gas_price)) / 2")
+    local has_enough=$(bc <<< "$dist_value > (1000000000*21000)") # let's assume we want at least enough for a tx at 1gwei
+    if [[ $has_enough -eq 0 ]]; then
+        echo "The address $eth_address does not have enough funds. Current balance is $balance and distribute value would be $dist_value" >&2
+        exit 1
+    fi
+
+    echo "sending $dist_value from $eth_address:$mnemonic_index to $eth_address_1:$mnemonic_index_1 and $eth_address_2:$mnemonic_index_2"
+    echo "current balance: $balance"
+
+    cast send --timeout 900 --legacy --mnemonic "$seed_mnemonic" --mnemonic-index "$mnemonic_index" --rpc-url "$rpc_url" --gas-price $gas_price --value $dist_value $eth_address_1 > /dev/null
+    cast send --timeout 900 --legacy --mnemonic "$seed_mnemonic" --mnemonic-index "$mnemonic_index" --rpc-url "$rpc_url" --gas-price $gas_price --value $dist_value $eth_address_2 > /dev/null
+}
+
+export -f distribute
+
+seq 0 32768 | parallel -j32 distribute {} $l1_rpc_url $bridge_address 0 &> r0-bridge-logs.log &
+seq 0 32768 | parallel -j32 distribute {} $l2_pp_url $bridge_address  1 &> r1-bridge-logs.log &
+seq 0 32768 | parallel -j32 distribute {} $l2_fep_url $bridge_address 2 &> r2-bridge-logs.log &
+
+# Check why cdk-node is running hot
+sudo /usr/sbin/profile-bpfcc --stack-storage-size 32000 -f -F 199 -p 3770750 100 > cdk-node-folded.out
+~/code/FlameGraph/flamegraph.pl --title="CDK Node Profile" cdk-node-folded.out > cdk-node.2.svg
