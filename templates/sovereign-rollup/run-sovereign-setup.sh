8a09a05c1c09c6a974acd0c2bdab1ae804d21131
Ji Hwan <jkim@polygon.technology>
2025-02-19 14:07:44 +0900

    chore: shellcheck fix
    Signed-off-by: Ji Hwan <jkim@polygon.technology>

--- templates/sovereign-rollup/run-sovereign-setup.sh
+++ templates/sovereign-rollup/run-sovereign-setup.sh
@@ -21,23 +21,23 @@ cp /opt/contract-deploy/sovereign-genesis.json /opt/zkevm-contracts/tools/create
 npx hardhat run ./tools/createNewRollup/createNewRollup.ts --network localhost
 # Extract the rollup manager address from the JSON file
 rollup_manager_addr="$(jq -r '.polygonRollupManagerAddress' /opt/zkevm/combined-001.json)"
-cast call --json --rpc-url  "{{.l1_rpc_url}}" $rollup_manager_addr 'rollupIDToRollupData(uint32)(address,uint64,address,uint64,bytes32,uint64,uint64,uint64,uint64,uint64,uint64,uint8)' 2 | jq '{"sovereignRollupContract": .[0], "sovereignChainID": .[1], "verifier": .[2], "forkID": .[3], "lastLocalExitRoot": .[4], "lastBatchSequenced": .[5], "lastVerifiedBatch": .[6], "_legacyLastPendingState": .[7], "_legacyLastPendingStateConsolidated": .[8], "lastVerifiedBatchBeforeUpgrade": .[9], "rollupTypeID": .[10], "rollupVerifierType": .[11]}' > /opt/zkevm-contracts/sovereign-rollup-out.json
+cast call --json --rpc-url  "{{.l1_rpc_url}}" "$rollup_manager_addr" 'rollupIDToRollupData(uint32)(address,uint64,address,uint64,bytes32,uint64,uint64,uint64,uint64,uint64,uint64,uint8)' 2 | jq '{"sovereignRollupContract": .[0], "sovereignChainID": .[1], "verifier": .[2], "forkID": .[3], "lastLocalExitRoot": .[4], "lastBatchSequenced": .[5], "lastVerifiedBatch": .[6], "_legacyLastPendingState": .[7], "_legacyLastPendingStateConsolidated": .[8], "lastVerifiedBatchBeforeUpgrade": .[9], "rollupTypeID": .[10], "rollupVerifierType": .[11]}' > /opt/zkevm-contracts/sovereign-rollup-out.json
 
 # These are some accounts that we want to fund for operations for running claims.
 bridge_admin_addr="{{.zkevm_l2_sovereignadmin_address}}"
 bridge_admin_private_key="{{.zkevm_l2_sovereignadmin_private_key}}"
 aggoracle_addr="{{.zkevm_l2_aggoracle_address}}"
-aggoracle_private_key="{{.zkevm_l2_aggoracle_private_key}}"
+# aggoracle_private_key="{{.zkevm_l2_aggoracle_private_key}}"
 claimtx_addr="{{.zkevm_l2_claimtx_address}}"
-claimtx_private_key="{{.zkevm_l2_claimtx_private_key}}"
+# claimtx_private_key="{{.zkevm_l2_claimtx_private_key}}"
 
 rpc_url="{{.op_el_rpc_url}}"
 # This is the default prefunded account for the OP Network
 private_key=$(cast wallet private-key --mnemonic 'test test test test test test test test test test test junk')
 
-cast send --value "{{.l2_funding_amount}}"  --rpc-url $rpc_url --private-key $private_key $bridge_admin_addr
-cast send --value "{{.l2_funding_amount}}"  --rpc-url $rpc_url --private-key $private_key $aggoracle_addr
-cast send --value "{{.l2_funding_amount}}" --rpc-url $rpc_url --private-key $private_key $claimtx_addr
+cast send --value "{{.l2_funding_amount}}"  --rpc-url $rpc_url --private-key "$private_key" $bridge_admin_addr
+cast send --value "{{.l2_funding_amount}}"  --rpc-url $rpc_url --private-key "$private_key" $aggoracle_addr
+cast send --value "{{.l2_funding_amount}}" --rpc-url $rpc_url --private-key "$private_key" $claimtx_addr
 
 # Contract Deployment Step
 cd /opt/zkevm-contracts || exit
@@ -50,17 +50,17 @@ libs = ['node_modules']" > foundry.toml
 echo "Building contracts with forge build"
 forge build contracts/v2/sovereignChains/BridgeL2SovereignChain.sol contracts/v2/sovereignChains/GlobalExitRootManagerL2SovereignChain.sol
 bridge_impl_nonce=$(cast nonce --rpc-url $rpc_url $bridge_admin_addr)
-bridge_impl_addr=$(cast compute-address --nonce $bridge_impl_nonce $bridge_admin_addr | sed 's/.*: //')
+bridge_impl_addr=$(cast compute-address --nonce "$bridge_impl_nonce" $bridge_admin_addr | sed 's/.*: //')
 ger_impl_addr=$(cast compute-address --nonce $((bridge_impl_nonce+1)) $bridge_admin_addr | sed 's/.*: //')
 ger_proxy_addr=$(cast compute-address --nonce $((bridge_impl_nonce+2)) $bridge_admin_addr | sed 's/.*: //')
 bridge_proxy_addr=$(cast compute-address --nonce $((bridge_impl_nonce+3)) $bridge_admin_addr | sed 's/.*: //')
 
 # This is one way to prefund the bridge. It can also be done with a deposit to some unclaimable network. This step is important and needs to be discussed
-cast send --value "{{.l2_funding_amount}}" --rpc-url $rpc_url --private-key $private_key $bridge_proxy_addr
+cast send --value "{{.l2_funding_amount}}" --rpc-url $rpc_url --private-key "$private_key" "$bridge_proxy_addr"
 forge create --broadcast --rpc-url $rpc_url --private-key $bridge_admin_private_key BridgeL2SovereignChain
-forge create --broadcast --rpc-url $rpc_url --private-key $bridge_admin_private_key GlobalExitRootManagerL2SovereignChain --constructor-args $bridge_proxy_addr
+forge create --broadcast --rpc-url $rpc_url --private-key $bridge_admin_private_key GlobalExitRootManagerL2SovereignChain --constructor-args "$bridge_proxy_addr"
 calldata=$(cast calldata 'initialize(address _globalExitRootUpdater, address _globalExitRootRemover)' $aggoracle_addr $aggoracle_addr)
-forge create --broadcast --rpc-url $rpc_url --private-key $bridge_admin_private_key TransparentUpgradeableProxy --constructor-args $ger_impl_addr $bridge_admin_addr $calldata
+forge create --broadcast --rpc-url $rpc_url --private-key $bridge_admin_private_key TransparentUpgradeableProxy --constructor-args "$ger_impl_addr" $bridge_admin_addr "$calldata"
 
 initNetworkID=2
 initGasTokenAddress=$(cast az)
@@ -72,8 +72,8 @@ initBridgeManager=$bridge_admin_addr
 initSovereignWETHAddress=$(cast az)
 initSovereignWETHAddressIsNotMintable=false
 
-calldata=$(cast calldata 'function initialize(uint32 _networkID, address _gasTokenAddress, uint32 _gasTokenNetwork, address _globalExitRootManager, address _polygonRollupManager, bytes _gasTokenMetadata, address _bridgeManager, address _sovereignWETHAddress, bool _sovereignWETHAddressIsNotMintable)' $initNetworkID $initGasTokenAddress $initGasTokenNetwork $initGlobalExitRootManager $initPolygonRollupManager $initGasTokenMetadata $initBridgeManager $initSovereignWETHAddress $initSovereignWETHAddressIsNotMintable)
-forge create --broadcast --rpc-url $rpc_url --private-key $bridge_admin_private_key TransparentUpgradeableProxy --constructor-args $bridge_impl_addr $bridge_admin_addr $calldata
+calldata=$(cast calldata 'function initialize(uint32 _networkID, address _gasTokenAddress, uint32 _gasTokenNetwork, address _globalExitRootManager, address _polygonRollupManager, bytes _gasTokenMetadata, address _bridgeManager, address _sovereignWETHAddress, bool _sovereignWETHAddressIsNotMintable)' $initNetworkID "$initGasTokenAddress" $initGasTokenNetwork "$initGlobalExitRootManager" "$initPolygonRollupManager" $initGasTokenMetadata $initBridgeManager "$initSovereignWETHAddress" $initSovereignWETHAddressIsNotMintable)
+forge create --broadcast --rpc-url $rpc_url --private-key $bridge_admin_private_key TransparentUpgradeableProxy --constructor-args "$bridge_impl_addr" $bridge_admin_addr "$calldata"
 
 jq --arg bridge_impl_addr "$bridge_impl_addr" '. += {"bridge_impl_addr": $bridge_impl_addr}' /opt/zkevm-contracts/sovereign-rollup-out.json > /opt/zkevm-contracts/sovereign-rollup-out.json.temp && mv /opt/zkevm-contracts/sovereign-rollup-out.json.temp /opt/zkevm-contracts/sovereign-rollup-out.json
 jq --arg ger_impl_addr "$ger_impl_addr" '. += {"ger_impl_addr": $ger_impl_addr}' /opt/zkevm-contracts/sovereign-rollup-out.json > /opt/zkevm-contracts/sovereign-rollup-out.json.temp && mv /opt/zkevm-contracts/sovereign-rollup-out.json.temp /opt/zkevm-contracts/sovereign-rollup-out.json
