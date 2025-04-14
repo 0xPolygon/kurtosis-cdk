f362606c934e2f6828cb93e60aefedc1fefee96e
Arpit Temani <temaniarpit27@gmail.com>
2025-04-14 10:51:56 -0600

    revert back

--- templates/contract-deploy/run-contract-setup.sh
+++ templates/contract-deploy/run-contract-setup.sh
@@ -100,34 +100,31 @@ sed -i 's#http://127.0.0.1:8545#{{.l1_rpc_url}}#' hardhat.config.ts
 # Set up a foundry project in case we do a gas token or dac deployment.
 printf "[profile.default]\nsrc = 'contracts'\nout = 'out'\nlibs = ['node_modules']\n" > foundry.toml
 
-echo_ts "{{ .gas_token_enabled }} ------ {{ .gas_token_address }}"
-# Deploy gas token
-if [[ "{{ .gas_token_enabled }}" == "true" || "{{ .gas_token_enabled }}" == true ]]; then
-    if [[ -z "{{ .gas_token_address }}" ]]; then
-        echo_ts "Deploying gas token to L1"
-        forge create \
-            --broadcast \
-            --json \
-            --rpc-url "{{ .l1_rpc_url }}" \
-            --mnemonic "{{ .l1_preallocated_mnemonic }}" \
-            contracts/mocks/ERC20PermitMock.sol:ERC20PermitMock \
-            --constructor-args "CDK Gas Token" "CDK" "{{ .zkevm_l2_admin_address }}" "1000000000000000000000000" \
-            > gasToken-erc20.json
-
-        jq \
-            --slurpfile c gasToken-erc20.json \
-            '.gasTokenAddress = $c[0].deployedTo' \
-            /opt/contract-deploy/create_rollup_parameters.json \
-            > /opt/zkevm-contracts/deployment/v2/create_rollup_parameters.json
-    else
-        echo_ts "Using L1 pre-deployed gas token: {{ .gas_token_address }}"
-        jq \
-            --arg c "{{ .gas_token_address }}" \
-            '.gasTokenAddress = $c' \
-            /opt/contract-deploy/create_rollup_parameters.json \
-            > /opt/zkevm-contracts/deployment/v2/create_rollup_parameters.json
-    fi
-fi
+# {{if .gas_token_enabled}}
+# {{if eq .gas_token_address ""}}
+echo_ts "Deploying gas token to L1"
+forge create \
+    --broadcast \
+    --json \
+    --rpc-url "{{.l1_rpc_url}}" \
+    --mnemonic "{{.l1_preallocated_mnemonic}}" \
+    contracts/mocks/ERC20PermitMock.sol:ERC20PermitMock \
+    --constructor-args "CDK Gas Token" "CDK" "{{.zkevm_l2_admin_address}}" "1000000000000000000000000" \
+    > gasToken-erc20.json
+jq \
+    --slurpfile c gasToken-erc20.json \
+    '.gasTokenAddress = $c[0].deployedTo' \
+    /opt/contract-deploy/create_rollup_parameters.json \
+    > /opt/zkevm-contracts/deployment/v2/create_rollup_parameters.json
+# {{else}}
+echo_ts "Using L1 pre-deployed gas token: {{ .gas_token_address }}"
+jq \
+    --arg c "{{ .gas_token_address }}" \
+    '.gasTokenAddress = $c' \
+    /opt/contract-deploy/create_rollup_parameters.json \
+    > /opt/zkevm-contracts/deployment/v2/create_rollup_parameters.json
+# {{end}}
+# {{end}}
 
 is_first_rollup=0 # an indicator if this deployment is doing the first setup of the agglayer etc
 if [[ ! -e /opt/zkevm/combined.json ]]; then
@@ -186,11 +183,9 @@ jq '.polygonZkEVML2BridgeAddress = .polygonZkEVMBridgeAddress' combined.json > c
 zkevm_global_exit_root_l2_address=$(jq -r '.genesis[] | select(.contractName == "PolygonZkEVMGlobalExitRootL2 proxy") | .address' /opt/zkevm/genesis.json)
 jq --arg a "$zkevm_global_exit_root_l2_address" '.polygonZkEVMGlobalExitRootL2Address = $a' combined.json > c.json; mv c.json combined.json
 
-echo_ts "{{ .gas_token_enabled }} ------ {{ .gas_token_address }}"
-
-if [[ "{{ .gas_token_enabled }}" == "true" || "{{ .gas_token_enabled }}" == true ]]; then
-    jq --slurpfile cru /opt/zkevm-contracts/deployment/v2/create_rollup_parameters.json '.gasTokenAddress = $cru[0].gasTokenAddress' combined.json > c.json; mv c.json combined.json
-fi
+# {{if .gas_token_enabled}}
+jq --slurpfile cru /opt/zkevm-contracts/deployment/v2/create_rollup_parameters.json '.gasTokenAddress = $cru[0].gasTokenAddress' combined.json > c.json; mv c.json combined.json
+# {{end}}
 
 
 # There are a bunch of fields that need to be renamed in order for the
