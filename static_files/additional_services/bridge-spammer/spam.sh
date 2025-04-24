961bd622a9f7f8ff181793ab13553626da13db6e
leovct <l009.vincent@gmail.com>
2025-04-24 11:58:13 +0200

    chore: check if env vars are defined

--- static_files/additional_services/bridge-spammer/spam.sh
+++ static_files/additional_services/bridge-spammer/spam.sh
@@ -2,9 +2,54 @@
 # This script simulates blockchain activity by perfoming L1 to L2 and L2 to L1 bridges.
 set -e
 
+# Checking environment variables.
+if [[ -z "${PRIVATE_KEY}" ]]; then
+  echo "Error: PRIVATE_KEY environment variable is not set"
+  exit 1
+fi
+if [[ -z "${L1_CHAIN_ID}" ]]; then
+  echo "Error: L1_CHAIN_ID environment variable is not set"
+  exit 1
+fi
+if [[ -z "${L1_RPC_URL}" ]]; then
+  echo "Error: L1_RPC_URL environment variable is not set"
+  exit 1
+fi
+if [[ -z "${L2_CHAIN_ID}" ]]; then
+  echo "Error: L2_CHAIN_ID environment variable is not set"
+  exit 1
+fi
+if [[ -z "${L2_RPC_URL}" ]]; then
+  echo "Error: L2_RPC_URL environment variable is not set"
+  exit 1
+fi
+if [[ -z "${L2_CLAIM_TX_MANAGER_ADDRESS}" ]]; then
+  echo "Error: L2_CLAIM_TX_MANAGER_ADDRESS environment variable is not set"
+  exit 1
+fi
+if [[ -z "${L1_BRIDGE_ADDRESS}" ]]; then
+  echo "Error: L1_BRIDGE_ADDRESS environment variable is not set"
+  exit 1
+fi
+if [[ -z "${L2_BRIDGE_ADDRESS}" ]]; then
+  echo "Error: L2_BRIDGE_ADDRESS environment variable is not set"
+  exit 1
+fi
+echo "PRIVATE_KEY: $PRIVATE_KEY"
+echo "L1_CHAIN_ID: $L1_CHAIN_ID"
+echo "L1_RPC_URL: $L1_RPC_URL"
+echo "L2_CHAIN_ID: $L2_CHAIN_ID"
+echo "L2_RPC_URL: $L2_RPC_URL"
+echo "L2_CLAIM_TX_MANAGER_ADDRESS: $L2_CLAIM_TX_MANAGER_ADDRESS"
+echo "L1_BRIDGE_ADDRESS: $L1_BRIDGE_ADDRESS"
+echp "L2_BRIDGE_ADDRESS: $L2_BRIDGE_ADDRESS"
+
+# Derive address from private key.
 eth_address=$(cast wallet address --private-key "$PRIVATE_KEY")
+echo "eth_address: $eth_address"
 
 # Fund claimtx manager.
+# TODO: This step should not be part of the spammer script, it should be done elsewhere.
 cast send --legacy --rpc-url "$L2_RPC_URL" --private-key "$PRIVATE_KEY" --value "10ether" "$L2_CLAIM_TX_MANAGER_ADDRESS"
 
 # Deposit on L1 to avoid negative balance.
