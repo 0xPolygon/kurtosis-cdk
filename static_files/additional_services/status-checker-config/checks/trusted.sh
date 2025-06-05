02a66c55fd6ef818b343b316685d82a57b08a620
minhd-vu <minhd_vu@yahoo.com>
2025-06-05 17:28:56 -0400

    fix: shellcheck (again)

--- static_files/additional_services/status-checker-config/checks/trusted.sh
+++ static_files/additional_services/status-checker-config/checks/trusted.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# shellcheck source=lib.sh
+# shellcheck source=./static_files/additional_services/status-checker-config/checks/lib.sh
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/lib.sh"
 
 if check_consensus rollup cdk_validium; then
