e3283cc992edf63366cb8a67c8562ccd69b67dfd
minhd-vu <minhd_vu@yahoo.com>
2025-06-06 10:05:45 -0400

    fix: wording

--- static_files/additional_services/status-checker-config/checks/lib.sh
+++ static_files/additional_services/status-checker-config/checks/lib.sh
@@ -1,7 +1,7 @@
 # shellcheck shell=bash
 # This file defines common utility functions used by multiple bash-based status
-# checks. It does NOT include a shebang so that the status-checker framework
-# skips executing this file directly.
+# checks. It does NOT include a shebang so that the status-checker skips
+# executing this file directly.
 
 check_consensus() {
   for consensus in "$@"; do
