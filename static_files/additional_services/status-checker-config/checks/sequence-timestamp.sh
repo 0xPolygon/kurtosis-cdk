9404d35ace8a65803896aadd0591f69e4bb3c86f
minhd-vu <minhd_vu@yahoo.com>
2025-06-05 22:41:07 -0400

    fix: shellcheck please

--- static_files/additional_services/status-checker-config/checks/sequence-timestamp.sh
+++ static_files/additional_services/status-checker-config/checks/sequence-timestamp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# shellcheck source=./static_files/additional_services/status-checker-config/checks/lib.sh
+# shellcheck source=static_files/additional_services/status-checker-config/checks/lib.sh
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/lib.sh"
 
 set -euo pipefail
