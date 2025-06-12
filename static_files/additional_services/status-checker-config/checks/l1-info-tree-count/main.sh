3bd4bf022c2954a36663c4373524c272ba73039e
minhd-vu <minhd_vu@yahoo.com>
2025-06-12 11:19:59 -0400

    fix: lint

--- static_files/additional_services/status-checker-config/checks/l1-info-tree-count/main.sh
+++ static_files/additional_services/status-checker-config/checks/l1-info-tree-count/main.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-CONSENSUS_CONTRACT_TYPE=cdk_validium
-L1_RPC_URL="http://$(kurtosis port print cdk el-1-geth-lighthouse rpc)"
-L2_RPC_URL="$(kurtosis port print cdk cdk-erigon-sequencer-001 rpc)"
-
-# CONSENSUS_CONTRACT_TYPE=rollup
-# L1_RPC_URL="https://ethereum.publicnode.com"
-# L2_RPC_URL="https://zkevm-rpc.com"
-
 cd "$(dirname "${BASH_SOURCE[0]}")"
 # shellcheck source=static_files/additional_services/status-checker-config/checks/lib.sh
 source "../lib.sh"
@@ -17,15 +9,8 @@ check_consensus rollup cdk_validium
 # Ensure that no more than $last_n_events batches sequenced can be processed
 # within the status-checker check interval.
 last_n_events=10
-rollup_contract="0x414e9E227e4b589aF92200508aF5399576530E4e"
-# rollup_contract="0x519E42c24163192Dca44CD3fBDCEBF6be9130987" # zkEVM mainnet rollup
-# rollup_contract=$(jq -r '.rollupAddress' /opt/zkevm/combined.json)
+rollup_contract=$(jq -r '.rollupAddress' /opt/zkevm/combined.json)
 virtual_batch_number=$(cast rpc --rpc-url "$L2_RPC_URL" zkevm_virtualBatchNumber | jq -r | cast to-dec)
-
-# latest_block_number=$(cast block-number --rpc-url $L1_RPC_URL) 
-# from_block=$((latest_block_number - 10000)) 
-# echo "latest_block: $latest_block_number"
-# echo "from_block: $from_block"
 
 events=$(
   cast logs "SequenceBatches(uint64,bytes32)" \
@@ -47,7 +32,7 @@ while IFS= read -r hex; do
   tx_hash=$(echo "$vb_json" | jq -r '.sendSequencesTxHash')
   tx_json=$(cast tx --json --rpc-url "$L1_RPC_URL" "$tx_hash")
   input_data=$(echo "$tx_json" | jq -r '.input')
-  batch_l2_data=$(echo $vb_json | jq -r '.batchL2Data')
+  batch_l2_data=$(echo "$vb_json" | jq -r '.batchL2Data')
 
   if is_consensus rollup; then
     l1_info_tree_leaf_count=$(
@@ -65,7 +50,14 @@ while IFS= read -r hex; do
     )
   fi
 
-  go run main.go "$batch_l2_data" \
-    | jq -e --argjson v "$l1_info_tree_leaf_count" 'all(.Blocks[]; .IndexL1InfoTree < $v)' 
+  # The Go script decodes the batch L2 data into JSON.
+  indexes=$(go run main.go "$batch_l2_data" | jq -r '.Blocks[] | .IndexL1InfoTree')
+
+  while IFS= read -r index; do
+    if (( index >= $l1_info_tree_leaf_count )); then
+      echo "ERROR: IndexL1InfoTree is >= L1InfoTreeLeafCount index=$index count=$l1_info_tree_leaf_count batch_number=$batch_number"
+      exit 1
+    fi
+  done <<< "$indexes"
 
 done <<< "$events" 
