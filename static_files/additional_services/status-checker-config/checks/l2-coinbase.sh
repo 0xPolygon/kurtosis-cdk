de1ec681b2f2269a4104cec6f8e54d02e8410bb8
minhd-vu <minhd_vu@yahoo.com>
2025-06-13 09:55:45 -0400

    fix: typo

--- static_files/additional_services/status-checker-config/checks/l2-coinbase.sh
+++ static_files/additional_services/status-checker-config/checks/l2-coinbase.sh
@@ -20,7 +20,7 @@ events=$(
 miner=$(cast block --rpc-url "$L2_RPC_URL" --json | jq -r '.miner')
 
 # Iterate over the sequence batches events because sometimes the batch number is
-# is greater than the virtual batch.
+# greater than the virtual batch.
 while IFS= read -r hex; do
   batch_number=$(printf "%s\n" "$hex" | cast to-dec)
 
