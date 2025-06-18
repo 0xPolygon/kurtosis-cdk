882960b8f0ad13f5b2ea4c5f393f75b75f625858
leovct <l009.vincent@gmail.com>
2025-06-18 13:09:51 +0200

    chore: same

--- static_files/additional_services/tx-spammer/spam.sh
+++ static_files/additional_services/tx-spammer/spam.sh
@@ -3,44 +3,53 @@ set -uo pipefail
 
 # This script simulates blockchain activity by sending transactions and making RPC calls.
 
+# Helper function to log messages in JSON format.
+log_error() {
+  echo "{\"error\": \"$1\"}"
+}
+
+log_info() {
+  echo "{\"info\": \"$1\"}"
+}
+
 # Function to handle errors and continue execution.
 handle_error() {
-  echo "An error occurred. Continuing execution..."
+  log_error "An error occurred. Continuing execution..."
 }
 trap handle_error ERR
 
 # Checking environment variables.
 if [[ -z "${PRIVATE_KEY}" ]]; then
-  echo "Error: PRIVATE_KEY environment variable is not set"
+  log_error "PRIVATE_KEY environment variable is not set"
   exit 1
 fi
 if [[ -z "${RPC_URL}" ]]; then
-  echo "Error: RPC_URL environment variable is not set"
+  log_error "RPC_URL environment variable is not set"
   exit 1
 fi
-echo "PRIVATE_KEY: $PRIVATE_KEY"
-echo "RPC_URL: $RPC_URL"
+log_info "PRIVATE_KEY: $PRIVATE_KEY"
+log_info "RPC_URL: $RPC_URL"
 
 # Sending load to the rpc.
 while true; do
-  echo "Sending transactions to the rpc..."
+  log_info "Sending transactions to the rpc"
   polycli loadtest \
     --rpc-url "$RPC_URL" \
     --private-key "$PRIVATE_KEY" \
     --legacy \
     --verbosity 700 \
-    --mode t,2 \
+    --mode t,2,7,v3 \
     --requests "50000" \
     --concurrency "5" \
     --rate-limit "50" \
     --eth-amount "0.000000000000000001"
 
-  echo "Making rpc calls..."
+  log_info "Making rpc calls"
   polycli rpcfuzz \
     --rpc-url "$RPC_URL" \
     --private-key "$PRIVATE_KEY" \
     --verbosity 700
 
-  echo "Waiting 60 seconds before sending more transactions..."
+  log_info "Waiting 60 seconds before sending more transactions"
   sleep 60
 done
