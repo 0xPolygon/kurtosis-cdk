75ef8dc1cc145832be5bd04996af53eb36bd2f3b
leovct <l009.vincent@gmail.com>
2025-06-19 15:15:08 +0200

    chore: reduce load

--- static_files/additional_services/tx-spammer/loadtest.sh
+++ static_files/additional_services/tx-spammer/loadtest.sh
@@ -38,7 +38,7 @@ while true; do
     --private-key "$PRIVATE_KEY" \
     --legacy \
     --requests "50000" \
-    --concurrency "30" \
+    --concurrency "10" \
     --rate-limit "50" \
     --eth-amount "1" \
     --pretty-logs=false
