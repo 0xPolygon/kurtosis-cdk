cadfdb1f27cab32ad3f20a07d867f72df3585cdc
leovct <l009.vincent@gmail.com>
2026-02-11 17:43:06 +0100

    chore: clean up

--- docker/agglayer-dev-ui/entrypoint.sh
+++ docker/agglayer-dev-ui/entrypoint.sh
@@ -13,9 +13,6 @@ npm run build
 # This makes the built static files available for nginx to serve.
 cp -r /app/dist/. /usr/share/nginx/html
 
-# After building, we only need the compiled artifacts, not the source.
-rm -rf /app
-
 # Start nginx in the foreground.
 # The 'daemon off' directive keeps nginx running as the main process,
 # preventing the container from exiting.
